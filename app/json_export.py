8c7a47734dd8f48109c05db6a296e2d61c19c55f
ManiDevL <Gladstone.H.Yevu@stud.leuphana.de>
2026-05-07 13:47:22 +0200

    Create json_export.py

---
+++ app/json_export.py
@@ -0,0 +1,17 @@
+def create_json_export():
+    """
+    This function will later create a JSON export with sustainability information.
+    """
+    return {
+        "name": "",
+        "CO2": "",
+        "NOX": "",
+        "Number_of_Electric_Vehicles": "",
+        "Impact": "",
+        "Risks": "",
+        "Opportunities": "",
+        "Strategy": "",
+        "Actions": "",
+        "Adopted_policies": "",
+        "Targets": ""
+    }
